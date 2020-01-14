2bacb4eddfe671c0a56039159d96f455a6da0f49
Artyom Rudakov <two_dollars@icloud.com>
2020-01-14 17:56:44 +0200

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "XXNibBridge",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(name: "XXNibBridge", targets: ["XXNibBridge"])
+    ],
+    targets: [
+        .target(
+            name: "XXNibBridge",
+            path: "XXNibBridge"
+        )
+    ]
+)
